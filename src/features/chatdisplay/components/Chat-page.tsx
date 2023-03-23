cd6936ace01a451caf1c025ccfdefc2ac8b44746
Shime01 <cesar.magana@student.csulb.edu>
2023-03-23 16:23:17 -0700

    started Chat_Page. seems like a copy from apple

--- src/features/chatdisplay/components/Chat-page.tsx
+++ src/features/chatdisplay/components/Chat-page.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Head = styled.h1`
+	font-size: 20px;
+	text-align: center;
+
+	border-radius: 8px;
+	border: 1px solid orange;
+	&:focus{
+		outline: none;
+		border-color: red
+	}
+`;
+
+const StringInput = styled.input`
+
+
+
+	border: 1px solid Green;
+	&:focus{
+		outline: none;
+		border-color: orange
+}
+`;
 
 export const Chat_page = () =>{
 
 
 	
 	return(
+	<>
 	<div>
-		
+		<Head>Messages</Head>
 		
 	</div>
+	<form>
+			<StringInput/>
+
+	</form>
+	</>
 	);
 }
